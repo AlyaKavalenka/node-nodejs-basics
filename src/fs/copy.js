f131ac1e2ec3929ea16a913b0f62a3e1effc062c
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-12 16:08:29 +0300

    feat: copy js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  const pathFrom = "src/fs/files";
+  const pathTo = "src/fs/files_copy";
+  const errorMessage = "FS operation failed";
+
+  if (fs.existsSync(pathTo) || !fs.existsSync(pathFrom)) {
+    throw new Error(errorMessage);
+  } else {
+    fs.cpSync(pathFrom, pathTo, {recursive: true});
+  }
 };
 
 await copy();
