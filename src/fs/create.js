dcda15630f46f58c4ccbde8b4bb10ba17a44d1f5
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-12 15:52:10 +0300

    feat: create js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const fileName = "src/fs/files/fresh.txt";
+  const dataInNewFile = "I am fresh and young";
+  const errorMessage = "FS operation failed";
+
+  if (fs.existsSync(fileName)) {
+    throw new Error(errorMessage);
+  } else {
+    fs.writeFileSync(fileName, dataInNewFile);
+  }
 };
 
 await create();
