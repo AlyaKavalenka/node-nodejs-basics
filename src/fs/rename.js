210882697178b986d7f8f4d46cd06ee742df96af
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-12 16:24:22 +0300

    feat: rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { throwError } from "./utils.js";
+
 const rename = async () => {
-    // Write your code here 
+  const oldName = "wrongFilename.txt";
+  const newName = "properFilename.md";
+  
+  const basePath = path.join("src", "fs", "files");
+  const oldNamePath = path.join(basePath, oldName);
+  const newNamePath = path.join(basePath, newName);
+
+  if (fs.existsSync(newNamePath) || !fs.existsSync(oldNamePath)) {
+    throwError();
+  } else {
+    fs.renameSync(oldNamePath, newNamePath);
+  }
 };
 
 await rename();
