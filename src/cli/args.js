faee83b45e3f771cd0005ff59ae99ee12f173273
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-13 20:38:11 +0300

    feat: args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const filterValue = "--";
+  const argsArr = process.argv;
+  const newArr = [];
+  argsArr.map((item, id, arr) => {
+    if (item.match(filterValue)) {
+      newArr.push([item.replace("--", ""), arr[id + 1]])
+    }
+  });
+  const result = newArr.map((item) => item.join(" is ")).join(", ");
+  console.log(result);
 };
 
 parseArgs();
