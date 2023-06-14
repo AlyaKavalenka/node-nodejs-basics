cd1b7c2d65801c9422549f198e67d4e15bb86ae3
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-14 04:32:20 +0300

    feat: read stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, '.', 'files', 'fileToRead.txt');
+
+  const stream = fs.createReadStream(filePath);
+  
+  stream.on("data", (chunk) => {
+    process.stdout._write(chunk);
+  });
 };
 
 await read();
