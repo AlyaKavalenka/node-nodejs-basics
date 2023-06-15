a1b9d02464bbe95685406a29caa94dda8f6c3a23
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-15 13:27:08 +0300

    feat: decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import { pipeline } from "stream";
+import { fileURLToPath } from 'url';
+import { createUnzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, '.', 'files', 'fileToCompress.txt');
+  const gzipFilePath = path.join(__dirname, '.', 'files', 'archive.gz');
+
+  const unzip = createUnzip();
+  const source = createReadStream(gzipFilePath);
+  const destination = createWriteStream(filePath);
+
+  pipeline(source, unzip, destination, (err) =>
+    err ? console.log(err.message) : true
+  );
 };
 
 await decompress();
