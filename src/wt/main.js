47dc6406a34005914ff5368be89ad7553d172ef9
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-16 16:02:50 +0300

    feat: wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads';
+import path, { resolve } from "path";
+import { fileURLToPath } from 'url';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, '.', 'worker.js');
+
+  const logicCores = os.cpus().length;
+  let initialValue = 10;
+
+  const promiseArr = [];
+  
+  for (let i = 0; i < logicCores; i += 1) {
+    promiseArr.push(new Promise((resolve, reject) => {
+      const worker = new Worker(filePath, {
+        workerData: initialValue,
+      });
+      worker.on("message", (msg) => resolve({ status: "resolved", data: msg }));
+      worker.on("error", () => reject({ status: "error", data: null }))
+    }))
+    initialValue++;
+  }
+
+  await Promise.all(promiseArr).then((values) => console.log(values));
 };
 
 await performCalculations();
